bd3eba83b1f09186f8dc44af66a5b85c7a338422
tmdtmd8588 <tmdtmd8588@gmail.com>
2025-09-27 22:17:37 +0900

    Update __init__.py

--- crawler_radhaha/__init__.py
+++ crawler_radhaha/__init__.py
@@ -1,7 +1,8 @@
-# touch crawler_radhaha/__init__.py # 배포때문에 잠깐 주석처리했어요
+""""
+touch crawler_radhaha/__init__.py # 배포때문에 잠깐 주석처리했어요
 git add crawler_radhaha/__init__.py
 git commit -m "chore: make crawler_radhaha a package"
 git push
-
+"""
 
 # 이 파일은 빈 내용이어도 ok / 이름형식만 필요 #
