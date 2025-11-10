167565605fd19ab0e34be4d5cdf1a3b8abb95abc
tmdtmd8588 <tmdtmd8588@gmail.com>
2025-11-10 17:10:20 +0900

    Update main_yolo.py

--- yolo/main_yolo.py
+++ yolo/main_yolo.py
@@ -10,6 +10,13 @@ import time
 from pydantic import BaseModel
 # from typing import List
 from yolo.beready_tracker import get_wait, start_tracker_thread
+import torch
+from ultralytics.nn.tasks import DetectionModel
+
+# PyTorch 2.6+에서 안전하게 DetectionModel 허용
+torch.serialization.add_safe_globals([DetectionModel])
+
+model = YOLO("yolov8n.pt")  # YOLOv8 모델 로드
 
 warnings.filterwarnings("ignore", category=FutureWarning)
 
@@ -166,6 +173,7 @@ app.include_router(router)
 if __name__ == "__main__":  # 현재 스크립트가 직접 실행될 때만 내부 코드를 실행
     uvicorn.run("main_yolo:app", reload=True)  # FastAPI 서버를 실행하는 명령
 """
+
 
 
 
